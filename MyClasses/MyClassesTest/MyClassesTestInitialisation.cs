2781471c87f0b84f147af41ded742993b6183661
mtchou <48898155+mtchou@users.noreply.github.com>
2021-09-11 21:36:59 +1000

    1. Create a new class: MyClassesTestInitialisation 2. Create two methods: AssemblyInitialise & AssemblyCleanUp

---
+++ MyClasses/MyClassesTest/MyClassesTestInitialisation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class MyClassesTestInitialisation
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialise(TestContext tc)
+        {
+            //TODO: Initialise for all tests within an assembly
+            tc.WriteLine("In AssmblyInitialise() method");
+        }
+
+        [AssemblyCleanup]
+        public static void AssembyCleanup()
+        {
+            //TODO: Clean up after all tests in an assembly
+        }
+    }
+}
