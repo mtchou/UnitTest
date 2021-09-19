65acff269f1b9e8e6449b57e3fdda434038c039d
mtchou <48898155+mtchou@users.noreply.github.com>
2021-09-19 18:09:51 +1000

    1. Create String Asser Class Test

---
+++ MyClasses/MyClassesTest/StringAssertClassTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyClassesTest
+{
+
+    [TestClass]
+    public class StringAssertClassTest : TestBase
+    {
+        [TestMethod]
+        public void ContainTest()
+        {
+            string str1 = "Mandy Chou";
+            string str2 = "Chou";
+
+            StringAssert.Contains(str1, str2);
+        }
+
+        [TestMethod]
+        public void StartWithTest()
+        {
+            string str1 = "All Lower Case";
+            string str2 = "All Lower";
+
+            StringAssert.StartsWith(str1, str2);
+        }
+
+        [TestMethod]
+        public void IsAllLowerCaseTest()
+        {
+            Regex r = new Regex(@"^([^A-Z])+$");
+            // zo+ means zo, zoo, zooooo etc, but not "z". {1, -}
+            //^ string start, $ string end
+            //[^A-Z] means anything that are not A-Z can be listed
+
+            StringAssert.Matches("all lower case 134@", r); //fail
+            //StringAssert.Matches("All lower case", r); //fail
+        }
+    }
+}
